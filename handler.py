5cf7179dea2a5665c568c0f7514fcdfa868bfdf8
Adam Smith <adam_smith3@comcast.com>
2021-03-11 15:24:41 -0500

    Refactor and cleanup

--- handler.py
+++ handler.py
@@ -6,72 +6,43 @@ from domain.objects import Candle, ThreeCandles
 from analysis import bullish_patterns_present, bearish_patterns_present
 
 
+def get_candle_package(symbol, candles):
+    c3 = candles[0]
+    c2 = candles[1]
+    c1 = candles[2]
+    candle3 = Candle(c3[0], symbol, c3[1], c3[2], c3[3], c3[4], c3[0])
+    candle2 = Candle(c2[0], symbol, c2[1], c2[2], c2[3], c2[4], c2[0])
+    candle1 = Candle(c1[0], symbol, c1[1], c1[2], c1[3], c1[4], c1[0])
+    return ThreeCandles(candle1, candle2, candle3)
+
+
 def main(event, context):
     sns = boto3.client('sns')
-    # dynamodb = boto3.resource('dynamodb')
     buys = []
     sells = []
     message = {}
-    # market_data = []
 
     event_message = json.loads(event['Records'][0]['Sns']['Message'])
-    # print(str(event))
-    # print(str(event_message))
-    # event_message = json.loads(event_message)
-    # print(str(event_message))
+
     data_type = event_message['data_type']
     exchange = event_message['exchange']
-    market_data = event_message['market_data']
-    # for entry in event_message['market_data']:
-    #     market_data.append(entry)
-    # print(str(market_data))
-    # symbols_string = event_message['currencies']
-    # symbols_string = symbols_string.lstrip('[')
-    # symbols_string = symbols_string.rstrip(']')
-    # symbols_string = symbols_string.replace('"', '')
-    # symbols_string.replace(" ", "")
-    # symbols = symbols_string.split(",")
-    # keys = []
-    # for symbol in symbols:
-    #     sym_dic = {"symbol": symbol.strip()}
-    #     keys.append(sym_dic)
-    # keys_dict = {"Keys": keys}
-    # responses = dynamodb.batch_get_item(
-    #     RequestItems={
-    #         'market-data': keys_dict
-    #     })
-    # market_data: list = responses['Responses']["market-data"]
-    for datum in market_data:
-        s = datum['symbol']
-        d = datum['data']
-        print(datum)
-        print(d)
-        d_list = json.loads(d)
-        print(d_list)
-        if len(d_list) == 0:
-            print("No data found for symbol: " + str(s))
+    market_data_list = event_message['market_data']
+
+    for market_data in market_data_list:
+        symbol = market_data['symbol']
+        data = json.loads(market_data['data'])
+        if len(data) == 0:
+            print("No data found for symbol: " + str(symbol))
         else:
-            last_candles = d_list[-3:]
-            print(last_candles)
-            candle_data3 = last_candles[0]
-            candle3 = Candle(candle_data3[0], s, candle_data3[1], candle_data3[2], candle_data3[3], candle_data3[4],
-                             candle_data3[0])
-            candle_data2 = last_candles[1]
-            candle2 = Candle(candle_data2[0], s, candle_data2[1], candle_data2[2], candle_data2[3], candle_data2[4],
-                             candle_data2[0])
-            candle_data1 = last_candles[2]
-            candle1 = Candle(candle_data1[0], s, candle_data1[1], candle_data1[2], candle_data1[3], candle_data1[4],
-                             candle_data1[0])
-            ccc = ThreeCandles(candle1, candle2, candle3)
+            last_candles = data[-3:]
+            ccc = get_candle_package(symbol, last_candles)
             if bullish_patterns_present(ccc):
-                buys.append(s)
+                buys.append(symbol)
             if bearish_patterns_present(ccc):
-                sells.append(s)
+                sells.append(symbol)
 
     print("Buys: " + str(buys))
-    print("sells: " + str(sells))
-    message['buys'] = buys
-    message['sells'] = sells
+    print("Sells: " + str(sells))
     sns.publish(
         TargetArn='arn:aws:sns:us-east-1:716418748259:trade-quantegy-data-soak',
         Message=json.dumps(message)
