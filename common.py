e97f26ae7432edf300c1792307a71edd2862ea74
Adam Smith <adam_smith3@comcast.com>
2021-03-24 13:50:28 -0400

    Carini exploration

--- common.py
+++ common.py
@@ -31,7 +31,7 @@ def get_target_arn(recv_topic_arn: str) -> str:
 
 
 def go(event, algorithm, algorithm_fn):
-    pprint(event)
+    # pprint(event)
     sns = boto3.client('sns')
     buys = []
     buy_prices = {}
