3b37ad52c21d3f620f4eeebd179dc6cf7ab63f95
Adam Smith <adam_smith3@comcast.com>
2021-03-30 13:38:16 -0400

    Convert macd signal to decimal for more granular comparison

--- common.py
+++ common.py
@@ -7,13 +7,16 @@ from domain.objects import Candle, ThreeCandles
 
 
 def get_candle_package(symbol, candles):
-    c3 = candles[0]
-    c2 = candles[1]
-    c1 = candles[2]
-    candle3 = Candle(c3[0], symbol, c3[1], c3[2], c3[3], c3[4], c3[0])
-    candle2 = Candle(c2[0], symbol, c2[1], c2[2], c2[3], c2[4], c2[0])
-    candle1 = Candle(c1[0], symbol, c1[1], c1[2], c1[3], c1[4], c1[0])
-    return ThreeCandles(candle1, candle2, candle3)
+    try:
+        c3 = candles[0]
+        c2 = candles[1]
+        c1 = candles[2]
+        candle3 = Candle(c3[0], symbol, c3[1], c3[2], c3[3], c3[4], c3[0])
+        candle2 = Candle(c2[0], symbol, c2[1], c2[2], c2[3], c2[4], c2[0])
+        candle1 = Candle(c1[0], symbol, c1[1], c1[2], c1[3], c1[4], c1[0])
+        return ThreeCandles(candle1, candle2, candle3)
+    except IndexError as e:
+        print("Exception thrown getting data for " + symbol)
 
 
 def get_env(recv_topic_arn: str) -> str:
