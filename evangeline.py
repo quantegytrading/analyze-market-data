323f67e5eb6287bc177783f9a238be6f6ec9616f
Adam Smith <adam_smith3@comcast.com>
2021-06-22 14:30:49 -0400

    Add Evangeline

--- evangeline.py
+++ evangeline.py
@@ -21,7 +21,19 @@ def evangeline(symbol, data) -> BuysSells:
     print(boll_ub)
     print("boll_lb")
     print(boll_lb)
-
+    bub_last_2 = boll_ub[-2:].values.tolist()
+    blb_last_2 = boll_lb[-2:].values.tolist()
+    print(symbol)
+    print(bub_last_2)
+    print(blb_last_2)
+    this_period = Decimal(bub_last_2[1])
+    last_period = Decimal(bub_last_2[0])
+    lthis_period = Decimal(blb_last_2[1])
+    llast_period = Decimal(blb_last_2[0])
+    print("this_period: " + str(this_period))
+    print("lthis_period: " + str(lthis_period))
+    print("last_period: " + str(last_period))
+    print("llast_period: " + str(llast_period))
     return BuysSells(buys, sells)
 
 
