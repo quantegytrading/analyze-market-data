2417ff1fb5f6ba4e5fa8752220332a9928bb7cfd
Adam Smith <adam_smith3@comcast.com>
2021-06-23 14:43:45 -0400

    evangeline dev

--- indicators/fibonacci.py
+++ indicators/fibonacci.py
@@ -2,7 +2,7 @@ def bullish_fibonacci(candles):
     for i in range(0, 7):
         candle = candles[i]
         print("close -5 -3 -2 -1")
-        print(candle.c + " " + candles[i + 5].c + " " + candles[i + 3].c + " " + candles[i + 2].c + " " + candles[i + 1].c)
+        print(str(candle.c) + " " + str(candles[i + 5].c) + " " + str(candles[i + 3].c) + " " + str(candles[i + 2].c) + " " + str(candles[i + 1].c))
         if candle.c < candles[i + 5].c and \
                 candle.c < candles[i + 3].c and \
                 candle.c < candles[i + 2].c and \
