a8cb732796af3ebd42e2ea39068935818341f53c
agsmith <agsmith11@gmail.com>
2025-01-07 14:45:46 -0500

    terraform

--- main.tf
+++ main.tf
@@ -23,7 +23,6 @@ resource "aws_lambda_function" "function" {
   handler                        = "evangeline.main"
   runtime                        = "python3.9"
   timeout                        = 900
-  reserved_concurrent_executions = 1
   memory_size                    = 128
   role                           = "arn:aws:iam::716418748259:role/quantegy-analyze-soak-us-east-1-lambdaRole"
 }
