3c2a2dad20bf7a60d48099fd0f8ff9ec224112f1
agsmith <agsmith11@gmail.com>
2025-01-14 16:43:57 -0500

    FreakyFreezy

--- src/freakyfreezy.py
+++ src/freakyfreezy.py
@@ -1,7 +1,7 @@
 from common import go, get_all_candle_packages
 # from indicators.candlestick_patterns import *
 from stockstats import StockDataFrame as sdf
-from appollonia import appolonia
+from apollonia import apollonia
 from bauhaus import bauhaus
 from carini import carini
 from dangermouse import dangermouse
@@ -13,20 +13,20 @@ from indicators.fibonacci import bullish_fibonacci, bearish_fibonacci
 
 
 def freakyfreezy(symbol, data) -> BuysSells:
-    appolonia_bs = appolonia(symbol, data)
+    apolonia_bs = apollonia(symbol, data)
     bauhaus_bs = bauhaus(symbol, data)
     carini_bs = carini(symbol, data)
     dangermouse_bs = dangermouse(symbol, data)
     evangeline_bs = evangeline(symbol, data)
-    buys = appolonia_bs.buys + bauhaus_bs.buys + carini_bs.buys + dangermouse_bs.buys + evangeline_bs.buys
-    sells = appolonia_bs.sells + bauhaus_bs.sells + carini_bs.sells + dangermouse_bs.sells + evangeline_bs.sells
+    buys = apollonia_bs.buys + bauhaus_bs.buys + carini_bs.buys + dangermouse_bs.buys + evangeline_bs.buys
+    sells = apollonia_bs.sells + bauhaus_bs.sells + carini_bs.sells + dangermouse_bs.sells + evangeline_bs.sells
     ff = BuysSells(set(buys), set(sells))
-    print("Appolonia Buys: " + str(appolonia_bs.buys))
+    print("Apollonia Buys: " + str(apollonia_bs.buys))
     print("Bauhaus Buys: " + str(bauhaus_bs.buys))
     print("Carini Buys: " + str(carini_bs.buys))
     print("Dangermouse Buys: " + str(dangermouse_bs.buys))
     print("Evangeline Buys: " + str(evangeline_bs.buys))
-    print("Appolonia Sells: " + str(appolonia_bs.sells))
+    print("Apollonia Sells: " + str(apollonia_bs.sells))
     print("Bauhaus Sells: " + str(bauhaus_bs.sells))
     print("Carini Sells: " + str(carini_bs.sells))
     print("Dangermouse Sells: " + str(dangermouse_bs.sells))
