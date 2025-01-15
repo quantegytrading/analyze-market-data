75807162620b31cb3f31768873eea3b9b72bbf8e
agsmith <agsmith11@gmail.com>
2025-01-14 19:39:04 -0500

    FreakyFreezy

--- src/freakyfreezy.py
+++ src/freakyfreezy.py
@@ -2,10 +2,10 @@ from common import go, get_all_candle_packages
 from apollonia import apollonia
 # from indicators.candlestick_patterns import *
 from stockstats import StockDataFrame as sdf
-# from .bauhaus import bauhaus
-# import carini
-# import dangermouse
-# import evangeline
+from bauhaus import bauhaus
+from bauhaus import carini
+from dangermouse import dangermouse
+from evangeline import evangeline
 import pandas as pd
 import datetime
 from domain.objects import BuysSells
@@ -14,23 +14,23 @@ from indicators.fibonacci import bullish_fibonacci, bearish_fibonacci
 
 def freakyfreezy(symbol, data) -> BuysSells:
     apollonia_bs = apollonia(symbol, data)
-    # bauhaus_bs = bauhaus.bauhaus(symbol, data)
-    # carini_bs = carini(symbol, data)
-    # dangermouse_bs = dangermouse(symbol, data)
-    # evangeline_bs = evangeline(symbol, data)
-    buys = []#apollonia_bs.buys + bauhaus_bs.buys #+ carini_bs.buys + dangermouse_bs.buys + evangeline_bs.buys
-    sells = []#apollonia_bs.sells + bauhaus_bs.sells #+ carini_bs.sells + dangermouse_bs.sells + evangeline_bs.sells
+    bauhaus_bs = bauhaus.bauhaus(symbol, data)
+    carini_bs = carini(symbol, data)
+    dangermouse_bs = dangermouse(symbol, data)
+    evangeline_bs = evangeline(symbol, data)
+    buys = apollonia_bs.buys + bauhaus_bs.buys + carini_bs.buys + dangermouse_bs.buys + evangeline_bs.buys
+    sells = apollonia_bs.sells + bauhaus_bs.sells + carini_bs.sells + dangermouse_bs.sells + evangeline_bs.sells
     ff = BuysSells(set(buys), set(sells))
-    # print("Apollonia Buys: " + str(apollonia_bs.buys))
-    # print("Bauhaus Buys: " + str(bauhaus_bs.buys))
-    # print("Carini Buys: " + str(carini_bs.buys))
-    # print("Dangermouse Buys: " + str(dangermouse_bs.buys))
-    # print("Evangeline Buys: " + str(evangeline_bs.buys))
-    # print("Apollonia Sells: " + str(apollonia_bs.sells))
-    # print("Bauhaus Sells: " + str(bauhaus_bs.sells))
-    # print("Carini Sells: " + str(carini_bs.sells))
-    # print("Dangermouse Sells: " + str(dangermouse_bs.sells))
-    # print("Evangeline Sells: " + str(evangeline_bs.sells))
+    print("Apollonia Buys: " + str(apollonia_bs.buys))
+    print("Bauhaus Buys: " + str(bauhaus_bs.buys))
+    print("Carini Buys: " + str(carini_bs.buys))
+    print("Dangermouse Buys: " + str(dangermouse_bs.buys))
+    print("Evangeline Buys: " + str(evangeline_bs.buys))
+    print("Apollonia Sells: " + str(apollonia_bs.sells))
+    print("Bauhaus Sells: " + str(bauhaus_bs.sells))
+    print("Carini Sells: " + str(carini_bs.sells))
+    print("Dangermouse Sells: " + str(dangermouse_bs.sells))
+    print("Evangeline Sells: " + str(evangeline_bs.sells))
     print("Buys: " + str(ff.buys))
     print("Sells: " + str(ff.sells))
     return ff
